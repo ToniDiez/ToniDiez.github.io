b861bf0afaf36869901a9e35b6c78732d24f0640
ToniDiez <74235672+ToniDiez@users.noreply.github.com>
2021-10-08 18:41:49 +0200

    Actualizado

---
+++ header.php
@@ -0,0 +1,21 @@
+<header>
+      <div class="fixed-top">
+<blockquote class="blockquote text-center">
+        <p class="mb-0"><u>Las abejas y sus aportaciones necesarias</u></p>
+        <img src="imagenes/abeja emoticono.png" class="rounded float-left" height=40px>
+        <img src="imagenes/abeja emoticono.png" class="rounded float-right" height=40px>
+      <tr>
+        
+        <ul class="text-center" id="pills-tab" role="tablist">
+          <img src= "imagenes/Logo de Abeja" class="rounded float-center" height=50px >
+        
+          <a  href="index.php" <?php if ($abeja=="index") echo " style='color:blue'"; ?>>-<u> Abejas </u>-</a>
+
+         <a href="index2.php" <?php if ($abeja=="index") echo " style='color:blue'"; ?>>-<u> Su importancia </u>-</a>
+
+          <a  href="index3.php" <?php if ($abeja=="index") echo " style='color:blue'"; ?>>-<u> Productos </u>-</a>
+          <img src= "imagenes/Logo de Abeja" class="rounded float-center" height=50px >
+    </tr>
+  </blockquote>
+  </div>
+  </header>
